208cd2d24ad4407a16a7171104439a96995095b4
dragono <dragono.subservice@pm.me>
2026-05-07 17:34:27 +0200

    Added LoRa code.

---
+++ lora/debug/send.py
@@ -0,0 +1,23 @@
+import serial
+import time
+
+PORT = "/dev/ttyACM0"
+BAUDRATE = 115200
+
+ser = serial.Serial(PORT, BAUDRATE, timeout=1)
+time.sleep(2)
+
+counter = 0
+
+while True:
+    message = f"Hello LoRa {counter}"
+    ser.write((message + "\n").encode("utf-8"))
+
+    print("Envoyé :", message)
+
+    response = ser.readline().decode("utf-8", errors="ignore").strip()
+    if response:
+        print("Feather :", response)
+
+    counter += 1
+    time.sleep(2)
